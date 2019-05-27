232f43e6a3c78f072cece35422d414b3e8df540c
Sebastian Arboleda <sebasarboleda22@gmail.com>
2019-05-27 12:25:57 -0400

    add big navbar logo before scrolling

--- static/js/main.js
+++ static/js/main.js
@@ -2,9 +2,13 @@ $(window).scroll(function() {
   if ($(this).scrollTop() > 1){  
     $('.navbar').addClass("navbar-custom");
     $('.navbar .navbar-brand img').attr('src','static/images/logo.png');
+    $('#desktop-logo').attr('height','60');
+    $('#desktop-logo').attr('style','margin-top:0');        
   }
   else {
     $('.navbar').removeClass("navbar-custom");
     $('#desktop-logo').attr('src','static/images/logo-white.png');
+    $('#desktop-logo').attr('height','200');    
+    $('#desktop-logo').attr('style','margin-top:4em');    
   }
 });
